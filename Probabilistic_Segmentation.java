3dc468a6a419aacce435d2871d89838b0262ed44
Jacques Boisvert <jacques.boisvert.1@gmail.com>
2013-09-23 02:34:16 -0400

    Full commented version

--- Probabilistic_Segmentation.java
+++ Probabilistic_Segmentation.java
@@ -3,7 +3,6 @@
  *
  */
 
-
 import java.awt.AWTEvent;
 import java.awt.TextField;
 import java.util.*;
@@ -13,21 +12,72 @@ import ij.process.*;
 import ij.plugin.filter.*;
 import ij.gui.*;
 
+
+/**The algorithm distinguishes signal from noise, requiring basically no
+ *parameter adjustment, and no prior knowledge about signal shape, making
+ *it robust and versatile, and, most of all, very convenient.
+ *Using the background filter option, it can do limited selection of small
+ *features over large features. It cannot distinguish overlapping features
+ *from one another. However, since the algorithm correctly identifies
+ *signal with high fidelity, its output can be used to replace the possibly
+ *unstable thresholding step of other algorithms. For example, to find
+ * spot-like features, probabilisticSegmentation can be used to identify the
+ * local intensity maxima in the image that correspond to actual signal.
+ * 
+ * Parameters : 
+ * bgFilter:filter mask size for median filter to subtract
+ *		    background. Filter size should be at least 1.5 times the diameter
+ *		    of  circular features, or 3 times the diameter of  linear features.
+ *			Must be odd.
+ *		    Currently, stacks are filters independantly.
+ *          Note that a local median filter is powerful and robust, but
+ *          slows down the code significantly. The current implementation is a slow implementation. 
+ *          Faster implementation TODO
+ * NOTE : bgFilter is most likely the only parameter that you'll have to
+ *        set.
+ *          
+ * fpExp : target maximum expected number of false positives per
+ * 		   image.  
+ * Note: For Gaussian noise and constant background, fpExp is
+ *       exact. For poisson noise on top of a strongly variable
+ *       background, fpExp might underestimate the number of false
+ *       positive pixels. Normally, the false positives will be
+ *       isolated, so if the features are sufficiently large, the false
+ *       positives can easily be eliminated by size. Also note that if
+ *       fpExp is very large or very small, the calculation for the
+ *       binomial probabilities saturates, meaning that different
+ *       extreme values of fpExp might yield the same segmentation
+ *       results.
+ * imgFilter: filter mask for determining true positives. Vector
+ *		    containing the filter size for all the dimensions of the filter
+ *		    mask. Must be odd.
+ *          Note: a larger imgFilter will be able to detect features of
+ *          lower SNR, as long as they're at least as large as the filter.
+ *          However, a larger filter also leads to an overestimation of the
+ *          feature size, which may have to be corrected via an erosion
+ *          step. A good alternative value for imgFilter with larger
+ *          features is [9 9]
+ * Please be advise that Probabilistic_Segmentation uses diverse function from the apache commons math library
+ * Commons Math is distributed under the terms of the Apache License, Version 2.0
+ * Commons Math requires JDK 1.5+ and has no runtime dependencies.
+ * @author Jonas Dorn, Jacques Boisvert
+ *
+ */
 public class Probabilistic_Segmentation implements ExtendedPlugInFilter,DialogListener{
 	private ImagePlus imp;
-	
+
 	private ImageStack segSt;//Segmentation stack.
 	private ImagePlus segImg;//Segmentation window.
-	
+
 	//Debug stacks
 	private ImageStack backgroundSt;
 	private ImageStack meanSt;
 	private ImageStack diffSt;
-	
-	
+
+
 	private int nPasses = 1;
 	private int pass;
-	
+
 	//private ImageStack stack;
 
 	//THRESHOLD PARAM
@@ -49,8 +99,8 @@ public class Probabilistic_Segmentation implements ExtendedPlugInFilter,DialogLi
 	private static final String evenKernelError = "Kernel must be of odd size";
 	final double VERSION = 1.04;
 
-	 private int flags = DOES_ALL|CONVERT_TO_FLOAT;
-	 
+	private int flags = DOES_ALL|CONVERT_TO_FLOAT;
+
 	public void run(ImageProcessor ip){
 
 		//Validate background/image filter.
@@ -119,7 +169,7 @@ public class Probabilistic_Segmentation implements ExtendedPlugInFilter,DialogLi
 
 		float noise = stdDiffImage/20;
 		//IJ.showMessage("NOISE : " + Float.toString(noise));
-		
+
 		//IJ.showMessage("Fp Threshold : " + Double.toString(fpThresh));
 		/**THRESHOLD
 		 * 
@@ -136,10 +186,10 @@ public class Probabilistic_Segmentation implements ExtendedPlugInFilter,DialogLi
 			if(debug){
 				ImagePlus backgroundImage = new ImagePlus("Median image",backgroundSt);
 				backgroundImage.show();
-				
+
 				ImagePlus diffImage = new ImagePlus("Diff Image",diffSt);
 				diffImage.show();
-				
+
 
 				ImagePlus meanImg = new ImagePlus("mean Image",meanSt);
 				meanImg.show();
@@ -148,8 +198,17 @@ public class Probabilistic_Segmentation implements ExtendedPlugInFilter,DialogLi
 		}
 		pass++;
 	}
+	/**This method is called by ImageJ for initialization
+	 * 
+	 * @param arg Unused here. 
+	 * @param imp The ImagePlus containing the image (or stack) to process.
+	 * @return    The method returns flags (i.e., a bit mask) specifying the
+	 *            capabilities (supported formats, etc.) and needs of the filter.
+	 *            See PlugInFilter.java and ExtendedPlugInFilter in the ImageJ
+	 *            sources for details.
+	 */
 	public int setup(java.lang.String arg, ImagePlus imp){
-		
+
 		this.imp = imp;
 		try{
 			if (imp.getType() == ImagePlus.COLOR_256 || imp.getType() == ImagePlus.COLOR_RGB){
@@ -167,6 +226,9 @@ public class Probabilistic_Segmentation implements ExtendedPlugInFilter,DialogLi
 		return flags;	
 	}
 
+	/**Called by imageJ after the call to setup
+	 * set up the parameter gui.
+	 */
 	public int showDialog(ImagePlus imp, String command, PlugInFilterRunner pfr) {
 		GenericDialog gd = new GenericDialog("Probabilistic Segmentation Parameters");
 		gd.addNumericField("False positive expectancy",fpExp,1);
@@ -193,7 +255,11 @@ public class Probabilistic_Segmentation implements ExtendedPlugInFilter,DialogLi
 		flags = IJ.setupDialog(imp, flags);     // ask whether to process all slices of stack (if a stack)
 		return flags;
 	}
-	
+
+	/**Called after modifications to the dialog.
+	 * Return true if valid
+	 * 
+	 */
 	public boolean dialogItemChanged(GenericDialog gd, AWTEvent e) {
 
 		Vector<?> numFields = gd.getNumericFields();
@@ -227,63 +293,12 @@ public class Probabilistic_Segmentation implements ExtendedPlugInFilter,DialogLi
 	}
 
 
-	public static void main(String [] args){
-		float [] dummy = {8,50,94,138,180,54,87,121,155,188,112,135,160,185,208,170,183,199,215,228,216,220,226,232,236};
-		printArray(dummy);
-		float med = QuickSelect.select(dummy, 0,dummy.length-1, dummy.length/2+1);
-		System.out.println("Median Found : " + med);
-		printArray(dummy);
-		System.out.println("Median Found : " + med);
-		System.out.println();
-		for(int i = 0;i<10;i++){
-			int [] arr = randomArray(0,40,25);
-			printArray(arr);
-			int median = QuickSelect.select(arr,0,arr.length-1,arr.length/2+1);
-			System.out.println("Median found : " + median);
-			printArray(arr);
-			System.out.println();
-		}
-	}
-
-
-	public static void printArray(int [] array){
-		System.out.print("[ " );
-		for(int i = 0; i<array.length;i++){
-			if (i == array.length-1)
-				System.out.print(array[i]);
-			else
-				System.out.print(array[i] + " , "); 
-		}
-		System.out.println(" ]");
-	}
-	public static void printArray(float [] array){
-		System.out.print("[ " );
-		for(int i = 0; i<array.length;i++){
-			if (i == array.length-1)
-				System.out.print(array[i]);
-			else
-				System.out.print(array[i] + " , "); 
-		}
-		System.out.println(" ]");
-	}
-	public static void printArray(short [] array){
-		System.out.print("[ " );
-		for(int i = 0; i<array.length;i++){
-			if (i == array.length-1)
-				System.out.print(array[i]);
-			else
-				System.out.print(array[i] + " , "); 
-		}
-		System.out.println(" ]");
-	}
-	public static int [] randomArray(int startRange,int endRange,int n){
-		int [] arr = new int [n];
-		for(int i = 0; i < n;i++){
-			arr[i] = startRange + (int)(Math.random() * ((endRange - startRange)+1));
-		}
-		return arr;
-	}
-
+	/**Function subtract calculates the subtraction of two float image processor
+	 * NOTE : The initial image is changed
+	 * @param destination - First image
+	 * @param source - image use for the subtraction
+	 * 
+	 */
 	public static void subtract(ImageProcessor destination, ImageProcessor source){
 		int width = destination.getWidth();
 		int height = destination.getHeight();
@@ -299,6 +314,12 @@ public class Probabilistic_Segmentation implements ExtendedPlugInFilter,DialogLi
 	}
 
 
+	/**Function diffCol calculates the nth difference between columns
+	 * Recursion is used for each subsequent difference.
+	 * @param destination - Float image processor
+	 * @param n - nth difference
+	 * @return diffCol - Float image processor of the nth difference
+	 */
 	private static ImageProcessor diffCol(ImageProcessor destination, int n){
 		int destWidth = destination.getWidth();
 		ImageProcessor diffIP = new FloatProcessor(destWidth-1,destination.getHeight());
@@ -319,6 +340,13 @@ public class Probabilistic_Segmentation implements ExtendedPlugInFilter,DialogLi
 		else
 			return diffCol(diffIP,n-1);
 	}
+
+	/**Function diffRow calculates the nth difference between rows.
+	 * Recursion is used for each subsequent difference. n-1,n-2...n-(n-1) 
+	 * @param destination - Float image processor
+	 * @param n - The nth difference
+	 * @return diffRow - Float image  processor of the nth difference
+	 */
 	private static ImageProcessor diffRow(ImageProcessor destination, int n){
 		int destWidth = destination.getWidth();
 		ImageProcessor diffIP = new FloatProcessor(destWidth,destination.getHeight()-1);
@@ -338,33 +366,16 @@ public class Probabilistic_Segmentation implements ExtendedPlugInFilter,DialogLi
 		else
 			return diffRow(diffIP,n-1);
 	}
-	public static ImageProcessor diff(ImageProcessor destination,int n){
-		ImageProcessor diffProcessor = new FloatProcessor(destination.getWidth()-1,destination.getHeight()-1);
-		int height = diffProcessor.getHeight();
-		int width = diffProcessor.getWidth();
-		int destWidth = destination.getWidth();
-		float [] diffPix = (float [])diffProcessor.getPixels();
-		float [] pix = (float [] )destination.getPixels();
 
-		for(int row = 0; row < height;row++){
-			int offset = row * width;
-			int diffOffset = row * destWidth;
-			for(int col = 0; col < width;col++){
-				//dim 1
-				float diff = pix[col+diffOffset+1] - pix[col+diffOffset];
-				//dim 2
-				diffPix[col+offset] = diff + (pix[col+diffOffset+destWidth] - pix[col+diffOffset]);
-			}
-		}
-		if(n == 1)
-			return diffProcessor;
-		else
-			return diff(diffProcessor,n-1);
-	}
-
+	/**Function that calculate the standard deviation of an image. 
+	 * 
+	 * @param ip - Image processor containing the pixels value for the image
+	 * @return the standard deviation
+	 */
 	private static float getStd(ImageProcessor ip){
+		//cut-off is roughly at 3 sigma, see Danuser, 1992 or Rousseeuw & Leroy, 1987
 		int k = 3;
-		double magicNumber = Math.pow(1.4826,2);
+		double magicNumber = Math.pow(1.4826,2);//see same publications
 		float [] pix = (float []) ip.getPixels();
 		//Find median
 		float median = QuickSelect.select(pix,0,pix.length-1,pix.length/2+1);
@@ -401,6 +412,12 @@ public class Probabilistic_Segmentation implements ExtendedPlugInFilter,DialogLi
 		return (float)Math.sqrt(sumRes / (nInlier-4) );
 	}
 
+	/**Function that return a byte image where pix[i,j] is 1 if pix[i,j] > threshold and 0 otherwise
+	 * 
+	 * @param ip - float image processor to threshold
+	 * @param thresh - threshold level
+	 * @return byte image processor with value 0|1
+	 */
 	private static ImageProcessor threshold(ImageProcessor ip,double thresh){
 		int height = ip.getHeight();
 		int width = ip.getWidth();
@@ -420,6 +437,13 @@ public class Probabilistic_Segmentation implements ExtendedPlugInFilter,DialogLi
 		return binaryIP;
 	}
 
+	/**Function that return a binary image where pix[i,j] is 255 if pix[i,j] > threshold and 0 otherwise
+	 * 
+	 * @param ip - float image processor to threshold
+	 * @param thresh - threshold level
+	 * @return binary image processor with value 0|255
+	 * See also threshold.
+	 */
 	private static ImageProcessor binaryThreshold(ImageProcessor ip, double thresh){
 		int height = ip.getHeight();
 		int width = ip.getWidth();
@@ -438,6 +462,10 @@ public class Probabilistic_Segmentation implements ExtendedPlugInFilter,DialogLi
 		return binaryIP;
 	}
 
+	/**Function that validate the background kernel input by the user
+	 * 
+	 * @return true if background kernel is > 3 and if odd
+	 */
 	private static boolean validateBgKernel(){
 		String bgTooSmall = "Background Filter must have a width/height > 3";
 
@@ -451,6 +479,12 @@ public class Probabilistic_Segmentation implements ExtendedPlugInFilter,DialogLi
 		}
 		return true;
 	}
+
+	/**Function that validate the image kernel given by the user
+	 * 
+	 * @return true if the image kernel is > 3 and if odd
+	 * @return false if the image kernel is < 3 and is even
+	 */
 	private static boolean validateImgKernel(){
 		String imgTooSmall = "Image Filter must have a width/height > 3";
 
@@ -464,14 +498,14 @@ public class Probabilistic_Segmentation implements ExtendedPlugInFilter,DialogLi
 		}
 		return true;
 	}
-	
 
-	
+	/** This method is called by ImageJ to set the number of calls to run(ip)
+	 *  corresponding to 100% of the progress bar */
 	public void setNPasses(int nPasses) {
 		this.nPasses = nPasses;
 		this.pass = 0;
 		// TODO Auto-generated method stub	
 	}
-		
+
 
 }
